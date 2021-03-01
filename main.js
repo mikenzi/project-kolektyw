e76cc1d5dccc8d9a8b9773d18acb3a9998fc24cf
mikenzi <kamil.mika1999@interia.pl>
2021-03-01 01:33:37 +0100

    header modified + burger menu

---
+++ main.js
@@ -0,0 +1,23 @@
+
+// BURGER
+
+const zmiana = document.querySelector(".burger")
+        const ico = document.querySelector('.ico')
+
+
+
+        zmiana.addEventListener("click", function () {
+            zmiana.classList.toggle("active")
+            ico.classList.toggle("active")
+
+        })
+//  NAV MENU 
+
+const menu = document.querySelector(".menu ul")
+const burgerButton = document.querySelector(".burger")
+
+function showMenu() {
+ menu.classList.toggle("activeBurger")
+}
+
+burgerButton.addEventListener("click", showMenu)
